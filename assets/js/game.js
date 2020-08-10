18530b7e2d1c1924fe1bae38e58a48ea60d51eb9
B. Drawe <brycerdrawe.b@gmail.com>
2020-08-09 21:26:43 -0600

    Updated some javascript

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string. Good for leaving yourself a string");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+// this creates a function named 'fight'
+function fight() {
+    window.alert('The fight has begun!');
+}
+//fight();
